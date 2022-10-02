b7643f6fa4aeb8b8d95f44627d5af8a3cc564724
Stian H. Johannesen <stianhj@gmail.com>
2022-10-03 01:34:28 +0200

    add font2 with emoji font

--- config.def.h
+++ config.def.h
@@ -6,6 +6,11 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Input:size=13:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"Noto Color Emoji:pixelsize=13:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
